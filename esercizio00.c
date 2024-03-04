e85c380d50e67266091bccff9701c3d0fb798642
Matteoasr1927 <162115955+Matteoasr1927@users.noreply.github.com>
2024-03-04 16:01:53 +0000

    salvataggiovincipela

---
+++ esercizio00.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+printf("chi tocca l'As Roma muore\n");   
+return(0);
+}
---
+++ esercizio02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+    char vincipela;
+    vincipela='V';
+    printf("%d\n",vincipela);
+}
