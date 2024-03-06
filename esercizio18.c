f9b9026932b1fb9faceee531cf5cc0e3dd3c8f67
Matteoasr1927 <162115955+Matteoasr1927@users.noreply.github.com>
2024-03-06 15:47:49 +0000

    xzxxzx

---
+++ esercizio18.c
@@ -0,0 +1,7 @@
+ #include 
+ int main()
+ {
+    int a;
+    printf("inserisci un numero");
+    scanf("%d",&a);
+    
---
+++ esercizio19.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int a, i;
+    int c=0;
+    printf("inserisci un numero");
+    scanf("%d", &a);
+    i=2;
+    while(i<a)
+    {
+        if(a%i==0)
+        {
+            c=c+1;
+        }
+        i=i+1;
+
+    }
+    if(c==0)
+{
+    printf("primo");
+
+}
+else
+{
+    printf("non primo");
+
+}
+}
